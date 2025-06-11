b9d9c471c0b798dbf5501074a2af338c644be5e3
youssefelhoubi <elhoubiyoussef@gmail.com>
2025-06-11 12:41:23 +0100

    made the log in methd with the resurs that I have

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,16 +1,26 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Res } from '@nestjs/common';
 import { UserService } from './user.service';
 import { user } from 'types/userTypes';
 import { signUPDto } from './DTO\'s/signUp.dto.ts';
+import { loginDTO } from './DTO\'s/logIn.dto';
+import { Response } from 'express';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) { }
   @Post()
-  signUP(@Body() user: signUPDto) {
+  signUP(@Body() user: signUPDto, @Res() res: Response) {
     try {
       const token = this.userService.signup(user);
-      return token
+      return res.status(200).json({token})
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  @Post()
+  logIn(@Body() user: loginDTO, @Res() res: Response) {
+    try {
+      
     } catch (error) {
       console.log(error);
     }
--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -13,4 +13,13 @@ export class UserService {
             console.log(error);
         }
     }
+    login() {
+        try {
+            // mkae the rest of the of the logic here 
+            const user = { id: 12323, type: "something " }
+            return this.jwtService.sign({ sub: { id: user.id, role: user.type } });
+        } catch (error) {
+            console.log(error);
+        }
+    }
 }
