dfc7ac180f9ff900b09325bce1871110d56b8e3c
youssefelhoubi <elhoubiyoussef@gmail.com>
2025-06-17 11:13:07 +0100

    made a schoole admin schema

--- backend/src/schemas/AdminEcole.schema.ts
+++ backend/src/schemas/AdminEcole.schema.ts
@@ -3,13 +3,12 @@ import { HydratedDocument } from 'mongoose';
 import * as mongoose from 'mongoose';
 import { User } from './user.schema';
 
-export type StudentDocument = HydratedDocument<Student>;
-// to make laater 
+export type AdminEcoleDocument = HydratedDocument<AdminEcole> ;
 
 @Schema()
-export class Student {
+export class AdminEcole {
   @Prop({ required: true, type: mongoose.Schema.Types.ObjectId, ref: "User" })
-  user_ID: User
+    user_ID: User
 }
 
-export const StudentSchema = SchemaFactory.createForClass(Student);
+export const AdminEcoleSchema = SchemaFactory.createForClass(AdminEcole);
