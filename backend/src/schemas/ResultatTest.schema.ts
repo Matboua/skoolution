00202ec5aa523722eef32e6b859b8ef47aa696dd
youssefelhoubi <elhoubiyoussef@gmail.com>
2025-06-17 16:24:40 +0100

    smale changes in one schema

--- backend/src/schemas/ResultatTest.schema.ts
+++ backend/src/schemas/ResultatTest.schema.ts
@@ -1,16 +1,18 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { HydratedDocument,Types } from 'mongoose';
 import * as mongoose from 'mongoose';
+import { Student } from './student.schema';
+import { Test } from './test.schema';
 
 export type ResultatTestDocument = HydratedDocument<ResultatTest> ;
 
 @Schema()
 export class ResultatTest {
-  @Prop({ type: Types.ObjectId, ref: 'Eleve', required: true })
-  eleve_id: Types.ObjectId;
+  @Prop({ type: Types.ObjectId, ref: 'Student', required: true })
+  eleve_id: Student;
 
   @Prop({ type: Types.ObjectId, ref: 'Test', required: true })
-  test_id: Types.ObjectId;
+  test_id: Test;
 
   @Prop({ required: true, type: Date })
   date_passage: Date;
