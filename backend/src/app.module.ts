681d6a7b929de9e6410f185f9acd1fb1b91def35
youssefelhoubi <elhoubiyoussef@gmail.com>
2025-06-18 15:39:50 +0100

    made new resource for student

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,6 +5,7 @@ import { UserModule } from './user/user.module';
 import { MailerModule } from '@nestjs-modules/mailer';
 import { MailerService } from './mailer/mailer.service';
 import { MongooseModule } from "@nestjs/mongoose"
+import { StudentModule } from './student/student.module';
 import * as dotenv from "dotenv";
 dotenv.config();
 @Module({
@@ -18,7 +19,7 @@ dotenv.config();
         pass: process.env.EMAIL_PASSWORD,
       },
     },
-  }),MongooseModule.forRoot("mongodb://localhost:27017/")],
+  }),MongooseModule.forRoot("mongodb://localhost:27017/"), StudentModule],
   controllers: [AppController],
   providers: [AppService, MailerService],
 })
