0cb4053e72514a12fcb44252b754dc40dd575559
youssefelhoubi <elhoubiyoussef@gmail.com>
2025-06-19 11:47:19 +0100

    injected some of the parent dependencies

--- backend/src/paret/paret.module.ts
+++ backend/src/paret/paret.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ParetService } from './paret.service';
 import { ParetController } from './paret.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Parent, ParentSchema } from 'src/schemas/parent.schema';
+import { User, UserSchema } from 'src/schemas/user.schema';
 
 @Module({
-  imports:[],
+  imports: [MongooseModule.forFeature([{ name: Parent.name, schema: ParentSchema }, { name: User.name, schema: UserSchema }])],
   controllers: [ParetController],
   providers: [ParetService],
 })
-export class ParetModule {}
+export class ParetModule { }
--- backend/src/paret/paret.service.ts
+++ backend/src/paret/paret.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { CreateParetDto } from './dto/create-paret.dto';
 import { UpdateParetDto } from './dto/update-paret.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Parent, ParentDocument } from 'src/schemas/parent.schema';
+import { Model } from 'mongoose';
+import { User, UserDocument } from 'src/schemas/user.schema';
+import { Mode } from 'fs';
 
 @Injectable()
 export class ParetService {
+  constructor(
+    @InjectModel(Parent.name) private ParentModel: Model<ParentDocument>,
+    @InjectModel(User.name) private UserModel: Model<UserDocument>
+  ) { }
   create(createParetDto: CreateParetDto) {
     return 'This action adds a new paret';
   }
