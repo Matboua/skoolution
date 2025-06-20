7e4e6e7f27ad981b5d75e5cf450c5f58b8213422
youssefelhoubi <elhoubiyoussef@gmail.com>
2025-06-20 12:02:11 +0100

    add Mongoose integration to AnneeScolaireModule for schema management

--- backend/src/annee_scolaire/annee_scolaire.module.ts
+++ backend/src/annee_scolaire/annee_scolaire.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AnneeScolaireService } from './annee_scolaire.service';
 import { AnneeScolaireController } from './annee_scolaire.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Annee_scolaire, Annee_scolaireSchema } from 'src/schemas/annee_scolaire.schema';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: Annee_scolaire.name, schema: Annee_scolaireSchema }])],
   controllers: [AnneeScolaireController],
   providers: [AnneeScolaireService],
 })
-export class AnneeScolaireModule {}
+export class AnneeScolaireModule { }
