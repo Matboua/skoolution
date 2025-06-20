17e9f6398a1a1e602937b3a716eb9fab90c7144c
youssefelhoubi <elhoubiyoussef@gmail.com>
2025-06-20 13:28:02 +0100

    add Parent, Echol, and Annee_scolaire schemas to StudentModule imports

--- backend/src/student/student.module.ts
+++ backend/src/student/student.module.ts
@@ -4,9 +4,17 @@ import { StudentController } from './student.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Student, StudentSchema } from 'src/schemas/student.schema';
 import { User, UserSchema } from 'src/schemas/user.schema';
+import { Parent, ParentSchema } from 'src/schemas/parent.schema';
+import { Echol, EcholSchema } from 'src/schemas/echol.schema';
+import { Annee_scolaire, Annee_scolaireSchema } from 'src/schemas/annee_scolaire.schema';
 
 @Module({
-  imports:[MongooseModule.forFeature([{ name: Student.name, schema: StudentSchema },{name:User.name,schema:UserSchema}])],
+  imports: [MongooseModule.forFeature([{ name: Student.name, schema: StudentSchema },
+  { name: User.name, schema: UserSchema },
+  { name: Parent.name, schema: ParentSchema },
+  { name: Echol.name, schema: EcholSchema },
+  { name: Annee_scolaire.name, schema: Annee_scolaireSchema }]
+  )],
   controllers: [StudentController],
   providers: [StudentService],
 })
