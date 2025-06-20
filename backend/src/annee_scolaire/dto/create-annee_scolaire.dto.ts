0c872296eaa1ec93e948b22b02f9f5117a67c6ed
youssefelhoubi <elhoubiyoussef@gmail.com>
2025-06-20 11:44:55 +0100

    add validation decorators to CreateAnneeScolaireDto for date fields

--- backend/src/annee_scolaire/dto/create-annee_scolaire.dto.ts
+++ backend/src/annee_scolaire/dto/create-annee_scolaire.dto.ts
@@ -1,1 +1,8 @@
-export class CreateAnneeScolaireDto {}
+import { IsAfterOneYear, IsnotBeforThisYear } from "helpers/datvalidator";
+
+export class CreateAnneeScolaireDto {
+    @IsnotBeforThisYear()
+    annee_scolaire: Date;
+    @IsAfterOneYear()
+    date_fin: Date;
+}
