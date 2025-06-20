7fd8b8e45a8b6913a6576373e91c613429a5ac7b
youssefelhoubi <elhoubiyoussef@gmail.com>
2025-06-20 13:35:00 +0100

    refactor StudentService to use CreateStudentDto and improve account creation logic

--- backend/src/student/student.service.ts
+++ backend/src/student/student.service.ts
@@ -3,6 +3,7 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Student, StudentDocument } from 'src/schemas/student.schema';
 import { Model } from "mongoose"
 import { User, UserDocument } from 'src/schemas/user.schema';
+import { CreateStudentDto } from './dto/Student.dto';
 
 @Injectable()
 export class StudentService {
@@ -10,11 +11,33 @@ export class StudentService {
         @InjectModel(Student.name) private StudentModel: Model<StudentDocument>,
         @InjectModel(User.name) private UserModel: Model<UserDocument>
     ) { }
-    complaitAccount(Student:any){
+    async complaitAccount(id: string, Student: CreateStudentDto) {
         try {
-            
+            // check if the student alredy exists
+            const existingStudent = await this.StudentModel.findOne({ user_ID: id });
+            if (existingStudent) {
+                return { success: false, message: 'Student already exists' };
+            }
+            // Create a new student
+            const newStudent = await this.StudentModel.create({
+                user_ID: id,
+                address: Student.address,
+                date_naissance: Student.date_naissance,
+                id_parent: Student.id_parent,
+                code_Massar: Student.code_Massar,
+                ecole: Student.ecole,
+                journal: Student.journal,
+                annee_scolaire_id: Student.annee_scolaire_id,
+                niveau_id: Student.niveau_id,
+                filiere_id: Student.filiere_id
+            })
+            if (!newStudent) {
+                return { success: false, message: 'Error creating student' };
+            }
+            return { success: true, message: 'Student created successfully', student: newStudent };
         } catch (error) {
             console.log(error);
+            return { success: false, message: 'Error creating account', error: error.message };
         }
     }
 }
