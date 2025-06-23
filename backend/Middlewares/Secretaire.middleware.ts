42f44b4f8caacb8b7cab5142466642940c4bdd85
youssefelhoubi <elhoubiyoussef@gmail.com>
2025-06-23 12:50:43 +0100

    add ESecretaireMiddleware to restrict access to Secretaire (Eleves)

---
+++ backend/Middlewares/Secretaire.middleware.ts
@@ -0,0 +1,14 @@
+import { Injectable, NestMiddleware, UnauthorizedException } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+
+@Injectable()
+export class ESecretaireMiddleware implements NestMiddleware {
+    use(req: Request, res: Response, next: NextFunction) {
+        if (!req['user'] || req['user'].type !== 'Elev') {
+            return res.status(403).json({
+                message: 'Access denied. This route is only accessible to Secretaire (Eleves).',
+            });
+        }
+        next();
+    }
+}
