5440fa29afc6c7d966818bce2f46b569a4d694c7
youssefelhoubi <elhoubiyoussef@gmail.com>
2025-06-23 12:51:46 +0100

    update EleveMiddleware to restrict access to Secretaire and add Super_AdminMiddleware for Super_Admin access control

--- backend/Middlewares/Super_Admin.middleware.ts
+++ backend/Middlewares/Super_Admin.middleware.ts
@@ -2,11 +2,11 @@ import { Injectable, NestMiddleware, UnauthorizedException } from '@nestjs/commo
 import { Request, Response, NextFunction } from 'express';
 
 @Injectable()
-export class EleveMiddleware implements NestMiddleware {
+export class Super_AdminMiddleware implements NestMiddleware {
     use(req: Request, res: Response, next: NextFunction) {
-        if (!req['user'] || req['user'].type !== 'Secretaire') {
+        if (!req['user'] || req['user'].type !== 'Super_Admin') {
             return res.status(403).json({
-                message: 'Access denied. This route is only accessible to Secretaire (Secretaire).',
+                message: 'Access denied. This route is only accessible to Super_Admin (Super_Admin).',
             });
         }
         next();
