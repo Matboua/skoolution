46aba7e8d856034065bb1295b00a9cc64a3b1b9d
youssefelhoubi <elhoubiyoussef@gmail.com>
2025-07-02 15:39:26 +0100

    fix: correct import paths for Test1 component

--- frontend/app/subjects/math/lessons/test1/page.tsx
+++ frontend/app/subjects/math/lessons/test1/page.tsx
@@ -1,6 +1,4 @@
-import Header from "@/app/components/client/Header";
-import Sidebar from "@/app/components/client/Sidebar";
-import Test1 from "@/app/components/client/testes/Test1";
+import Test1 from "../../../../components/client/testes/Test1";
 
 export default function page() {
 	return <Test1 />;
