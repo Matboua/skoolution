9611d923eaba76252aec48bd3f80eda03917da05
youssefelhoubi <elhoubiyoussef@gmail.com>
2025-07-07 13:00:10 +0100

    feat: implement login function for user signup with form data handling

--- frontend/actions/signUp.ts
+++ frontend/actions/signUp.ts
@@ -1,4 +1,21 @@
 import { axiosConfig } from "../config/axiosConfig";
+import { signupFormData } from "../types/formDatas";
 
 
-const login = async () => {}
+export const login = async (FormData: signupFormData) => {
+    try {
+        const response = await axiosConfig.post("/user/signup", {
+            nom: FormData.nom,
+            prenom: FormData.prenom,
+            mail: FormData.mail,
+            pwd: FormData.pwd,
+            ville: FormData.ville,
+            tel: FormData.tel,
+            type: FormData.type
+        });
+        return response.data;
+    } catch (error) {
+        console.error("Sign-up failed:", error);
+        throw error;
+    }
+}
--- frontend/types/formDatas.ts
+++ frontend/types/formDatas.ts
@@ -2,3 +2,12 @@ export type LogInFormData = {
     email: string;
     password: string;
 };
+export interface signupFormData {
+    nom: string
+    prenom: string
+    mail: string
+    pwd: string
+    ville: string
+    tel: string
+    type: string
+}
